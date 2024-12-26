ee9442f5c56e4d43fc485bd8db7eeef6724fa508
Raj Kupekar <kupekarraj@gmail.com>
2024-12-26 11:20:36 +0000

    Commented the sample Exception Code

--- src/exception.py
+++ src/exception.py
@@ -26,9 +26,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == "__main__":
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.error("Divide by zero error")
-        raise CustomException(e, sys)  # Correctly raising the exceptions
+# if __name__ == "__main__":
+#     try:
+#         a = 1 / 0
+#     except Exception as e:
+#         logging.error("Divide by zero error")
+#         raise CustomException(e, sys)  # Correctly raising the exceptions
