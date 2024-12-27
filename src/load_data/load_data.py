8802460859a9d1eab3e4c118832f6f343d62f260
Raj Kupekar <kupekarraj@gmail.com>
2024-12-27 18:52:55 +0000

    Coded the Load Data Module

---
+++ src/load_data/load_data.py
@@ -0,0 +1,28 @@
+# Import required libraries
+import pandas as pd
+import sys
+from src.logger import logging
+from src.exception import CustomException
+
+class DataLoader:
+    def __init__(self,filepath):
+        self.filepath= filepath
+
+    def load_data(self):
+        logging.info("Data Loading Started")
+        try:
+            df= pd.read_csv(self.filepath)
+            # Dropping rows having null values and storing the output the same dataset
+            df.dropna(subset=["trading_name"], inplace=True)
+            return df
+            logging.info("Data Loading Complete")
+
+        except Exception as e:
+            logging.error("Error in Data Loading")
+            raise CustomException(e,sys)
+        
+if __name__ == '__main__':
+    file_path= input().strip()
+    data_loader= DataLoader(file_path)
+
+
